134f6923c7c343de50e79cb8cd2092dd3fe15683
Vikram Jha <jhavikram11121998@gmail.com>
2025-09-02 15:01:07 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -99,6 +99,7 @@ app.post("/login", async(req, res)=>{
         return res.status(200).json({message: "Login successful"});
     }catch(err){
         console.log(err);
+        res.status(500).json({errorMessage: "Internal sercer error"});
     }
 });
 
